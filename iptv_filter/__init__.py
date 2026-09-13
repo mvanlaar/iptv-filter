f0dd2682b106a120d943cde19d99b6d35ca46abf
Martijn van Laar <martijn@mvanlaar.net>
2026-09-13 10:50:07 +0200

    Add version number to __init__.py

--- iptv_filter/__init__.py
+++ iptv_filter/__init__.py
@@ -3,3 +3,5 @@
 # # Prevent django reload thread from running config!
 # if os.environ.get('RUN_MAIN', None) != 'true':
 #     default_app_config = 'iptv_filter.apps.IptvFilterConfig'
+
+__version__ = "1.0.0"
